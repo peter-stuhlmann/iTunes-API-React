d49fd7ee8e989ddeca2ea121a8b35f784fbe9273
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-06 11:59:05 +0200

    Add Router and import components
    Import BrowserRouter, Switch and Route from react-router-dom (for multiple routes);
    import the components TrackList, LegalNotice, PrivacyPolicy and Footer and add them to the render function of App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import TrackList from './TrackList';
+import LegalNotice from './LegalNotice';
+import PrivacyPolicy from './PrivacyPolicy';
+import Footer from './Footer';
 
 export default class App extends Component {
   
   render() {
     return (
-      <React.Fragment>
+      <Router>
 
-      </React.Fragment>
+        <span className="site-title">Songs</span>
+
+        <Switch>
+          <Route exact path="/" component={ TrackList } />
+          <Route path="/impressum" component={ LegalNotice } />
+          <Route path="/datenschutzerklaerung" component={ PrivacyPolicy } />
+        </Switch>
+
+        <Footer />
+
+      </Router>
     );
   }
 }
