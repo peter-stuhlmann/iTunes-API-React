1a72e937f17dc6fb123f21daa44c97869273aad3
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-06 11:55:18 +0200

    Create a component file and add legal notice

---
+++ src/components/LegalNotice.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import MetaTags from 'react-meta-tags';
+
+export default class LegalNotice extends Component {
+    render() {
+      return (
+        <React.Fragment>
+
+          <MetaTags>
+            <title>Impressum | iTunes Tracklist</title>
+            <link rel="shortcut icon" type="image/png" href={require("../assets/img/powered.png")} />
+            <meta name="description" content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua." />
+          </MetaTags>
+
+          <article>
+            <h1>Impressum (Legal Notice)</h1>
+
+            <section>
+              <h2>Angaben gemäß § 5 TMG</h2>
+              <p>Peter R. Stuhlmann<br />Martha-Arendsee-Str. 10<br />12681 Berlin</p>
+              <p>Telefon: +49 (0) 1578 5630944<br />E-Mail: info@peter-stuhlmann.de</p>
+              <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+            </section>
+
+            <section>
+              <h2>Haftung für Inhalte</h2>
+              <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+            </section>
+
+            <section>
+              <h2>Haftung für Links</h2>
+              <p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+            </section>
+
+            <section>
+              <h2>Urheberrecht</h2>
+              <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+              <p>Quelle: https://www.e-recht24.de</p>
+            </section>
+          </article>
+
+        </React.Fragment>
+    );
+  }
+}
